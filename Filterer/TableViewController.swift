48d480d95cca5e53eb189b577fde389905223dc9
Samir <samir@samirs-mac.local>
2016-01-18 08:35:51 +0100

    added buttons and tableview

---
+++ Filterer/TableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TableViewController.swift
+//  Filterer
+//
+//  Created by Samir on 16/01/16.
+//  Copyright © 2016 UofT. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
+    
+    let filters = [
+        "Green",
+        "Yellow",
+        "Blue",
+        "Red"
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(filters[indexPath.row])
+    }
+    
+    // populraion of table
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FilterCell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = filters[indexPath.row]
+        
+        return cell
+    }
+}
--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -8,63 +8,134 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var filteredImage: UIImage?
     
-    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet var imgView: UIImageView!
     
-    @IBOutlet weak var imageToggle: UIButton!
+    @IBOutlet var secondaryMenu: UIView!
+    @IBOutlet var bottomMenu: UIView!
     
-    @IBAction func onImageToggle(sender: UIButton) {
-        if imageToggle.selected {
-            let beforeImage = UIImage(named: "scenery")!
-            imgView.image = beforeImage
-            imageToggle.selected = false
-        } else {
-            imgView.image = filteredImage
-            imageToggle.selected = true
-        }
-    }
+    @IBOutlet var filterButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        let image = UIImage(named: "scenery")!
-        
-        let rgbaImage = RGBAImage(image: image)!
-        
-        let avgRed = 107
-        
-        for y in 0..<rgbaImage.height {
-            for x in 0..<rgbaImage.width {
-                let index = y * rgbaImage.width + x
-                
-                var pixel = rgbaImage.pixels[index]
-                
-                let redDelta = Int(pixel.red) - avgRed
-                
-                var modifier = 1 + 4 * (Double(y) / Double(rgbaImage.height))
-                if(Int(pixel.red) < avgRed) {
-                    modifier = 1
-                }
-                
-                pixel.red = UInt8(max(min(255,
-                    Int(round(Double(avgRed) + modifier *
-                    Double(redDelta)))), 0))
-                rgbaImage.pixels[index] = pixel
-            }
-            
-            filteredImage = rgbaImage.toUIImage()
-        }
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+        secondaryMenu.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onShare(sender: AnyObject) {
+        let activityController = UIActivityViewController(activityItems: ["check this out", imgView.image!], applicationActivities: nil)
+        presentViewController(activityController, animated: true, completion: nil)
+    }
+    
 
-
+    @IBAction func onNewPhoto(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: .ActionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: {
+            action in
+            if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
+                self.showCamera()
+            } else {
+                self.showNoCamera()
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Album", style: .Default, handler: {
+            action in
+            self.showAlbum()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+    }
+    
+    func showCamera()
+    {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .Camera
+        
+        self.presentViewController(cameraPicker, animated: true, completion: nil)
+        
+    }
+    
+    func showNoCamera () {
+        let alertNoCamera = UIAlertController(title: "No Camera",
+            message: "The device has no camera",
+            preferredStyle: .Alert)
+        let pressOK = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        alertNoCamera.addAction(pressOK)
+        
+        self.presentViewController(alertNoCamera, animated: true, completion: nil)    }
+    
+    // dismiss after cancel action
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imgView.image = image
+        }
+        
+    }
+    
+    func showAlbum() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .PhotoLibrary
+        
+        self.presentViewController(cameraPicker, animated: true, completion: nil)    }
+    
+    @IBAction func onFilter(sender: UIButton) {
+        if (sender.selected) {
+            hideSecondaryMenu()
+            sender.selected = false
+        } else {
+            showSecondaryMenu()
+            sender.selected = true
+        }
+    }
+    
+    func showSecondaryMenu() {
+        
+        view.addSubview(secondaryMenu)
+        
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraintEqualToAnchor(bottomMenu.topAnchor)
+        let heightConstraint = secondaryMenu.heightAnchor.constraintEqualToConstant(44)
+        let leftConstraint = secondaryMenu.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+        
+        NSLayoutConstraint.activateConstraints([bottomConstraint, heightConstraint, leftConstraint, rightConstraint])
+        
+        view.layoutIfNeeded()
+        
+        self.secondaryMenu.alpha = 0
+        UIView.animateWithDuration(0.4) {
+            self.secondaryMenu.alpha = 1.0
+        }
+    }
+    
+    func hideSecondaryMenu() {
+        
+        UIView.animateWithDuration(0.4, animations: {
+              self.secondaryMenu.alpha = 0
+                } ) { completed in
+                    if completed == true {
+                        self.secondaryMenu.removeFromSuperview()
+                    }
+                
+        }
+    }
 }
 
